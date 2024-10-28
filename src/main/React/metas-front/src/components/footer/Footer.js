ebeabe5ba77d92553468ffe409728e5355d14110
Sofia Valadares Cavalcanti <svc2@cesar.org.br>
2024-10-28 13:46:11 -0300

    header and footer

--- src/main/React/metas-front/src/components/footer/Footer.js
+++ src/main/React/metas-front/src/components/footer/Footer.js
@@ -1,13 +1,29 @@
 import './Footer.css';
 
 function Footer() {
+    const handleImageClick = () => {
+        window.location.href = "https://www.gov.br/saude/pt-br";
+    };
     return (
         <div className='footer'>
-            <div className='footer_icons'>
-                <img src='/icons/icon-fone.png' alt='Email'/>
+            <div className="footer_icons">
+                <div className="footer_icon">
+                    <img src="/icons/icon-email.png" alt="Ícone de Email"/>
+                    <p>andreza.guimaraes@outlook.com</p>
+                </div>
+
+                <div className="footer_icon">
+                    <img src="/icons/icon-fone.png" alt="Ícone de Telefone"/>
+                    <p>(87) 99995-1608</p>
+                </div>
             </div>
+
+
+            <img src="/images/SUS-logo.png" alt="Ministério da Saúde" onClick={handleImageClick} />
+
+
         </div>
     );
-};
+}
 
 export default Footer;
