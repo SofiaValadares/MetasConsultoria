b82988da8e4f11fe666ccc6f9493c6e7d1b197de
Sofia Valadares Cavalcanti <svc2@cesar.org.br>
2024-11-08 11:55:11 -0300

    CRUD Collaborator

--- src/main/java/com/metasconsultoria/entities/NextStep.java
+++ src/main/java/com/metasconsultoria/entities/NextStep.java
@@ -7,29 +7,5 @@ public class NextStep {
     private int idNextStep;
     private String nextStep;
 
-    public NextStep() {
 
-    }
-
-    public int getIdNextStep() {
-        return idNextStep;
-    }
-
-    public void setIdNextStep(int idNextStep) {
-        this.idNextStep = idNextStep;
-    }
-
-    public String getNextStep() {
-        return nextStep;
-    }
-
-    public void setNextStep(String nextStep) {
-        this.nextStep = nextStep;
-    }
-
-    public static NextStep getNextStepById(Connection conn, int id) {
-        NextStep nextStep = null;
-
-        return nextStep;
-    }
 }
