5b11323df9043075dc926c7961b4a4706a5023c3
Sofia Valadares Cavalcanti <svc2@cesar.org.br>
2024-11-08 11:00:38 -0300

    UserCRUD

--- src/main/java/com/metasconsultoria/entities/User.java
+++ src/main/java/com/metasconsultoria/entities/User.java
@@ -16,6 +16,13 @@ public class User {
 
     }
 
+    public User(int idUser, String name, String password, String email) {
+        this.idUser = idUser;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
     public int getIdUser() {
         return idUser;
     }
@@ -47,52 +54,4 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
-
-    public static User getUserById(Connection conn, int id) {
-        User user = null;
-        String sql = "SELECT * FROM User WHERE cod_user = ?";
-
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setInt(1, id);
-            try (ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
-                    user = new User();
-                    user.setIdUser(rs.getInt("cod_user"));
-                    user.setName(rs.getString("name"));
-                    user.setEmail(rs.getString("email"));
-                    user.setPassword(rs.getString("password"));
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(); // Ou use uma abordagem de tratamento de erro mais robusta
-        }
-
-        return user;
-    }
-
-    public static User getUserByLogin(Connection conn, String email, String password) {
-        User user = null;
-        String sql = "SELECT cod_user, name, email, password FROM User WHERE email = ? AND password = ?";
-
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, email);
-            stmt.setString(2, password);
-
-            try (ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
-                    user = new User();
-                    user.setIdUser(rs.getInt("cod_user"));
-                    user.setName(rs.getString("name"));
-                    user.setEmail(rs.getString("email"));
-                    user.setPassword(rs.getString("password"));
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(); // Ou use uma abordagem de tratamento de erro mais robusta
-        }
-
-        return user;
-    }
-
 }
